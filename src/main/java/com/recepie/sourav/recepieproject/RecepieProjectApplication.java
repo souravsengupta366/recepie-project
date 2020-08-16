e95f6ec01e19217948862b39976293b7cfc000df
sourav <souravsengupta366@gmail.com>
2020-08-16 16:43:54 +0530

    first commit

---
+++ src/main/java/com/recepie/sourav/recepieproject/RecepieProjectApplication.java
@@ -0,0 +1,13 @@
+package com.recepie.sourav.recepieproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecepieProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecepieProjectApplication.class, args);
+	}
+
+}
